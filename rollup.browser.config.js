d16c6a9b9f574ba77cbd57978bd2b5c03955adb5
Hugo Alliaume <kocal@live.fr>
2019-04-06 11:07:10 +0200

    Bundle Twing
    I hope it works :)

--- rollup.browser.config.js
+++ rollup.browser.config.js
@@ -1,6 +1,10 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import alias from "rollup-plugin-alias";
+import json from 'rollup-plugin-json';
 import typescript from 'rollup-plugin-typescript2';
-import pkg from './package.json';
 import { terser } from "rollup-plugin-terser";
+import pkg from './package.json';
 
 export default {
   input: 'src/index.ts',
@@ -8,14 +12,21 @@ export default {
     {
       file: 'dist/browser.min.js',
       format: 'umd',
-      name: 'TwigParser'
+      name: 'TwigParser',
     },
   ],
   external: [
-    ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
+    resolve({
+      browser: true,
+    }),
+    commonjs(),
+    alias({
+      twing: require.resolve('twing/dist/lib.min.js'),
+    }),
+    json(),
     typescript({
       typescript: require('typescript'),
     }),
